aaddf388bd7acb61fa693fa6262efe00637fd6c8
Adam Dudley <dudleyadam@gmail.com>
2018-11-21 19:16:44 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -37,7 +37,7 @@ end
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = number_of_times
-  for counter in number_of_times.each do
+  for counter in number_of_times.each
   end
     puts phrase
     #counter += 1
