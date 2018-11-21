17d2e00ce7c6e6aa669f5f4376f9df94856e7ee8
Adam Dudley <dudleyadam@gmail.com>
2018-11-21 18:37:24 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -19,7 +19,7 @@ end
 def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
-  while number_of_times >= 1
+  while counter >= 1
     puts phrase
     break if counter < 1
     counter += 1
